c8bea5a8742833d17e01988672610dbf7440ed48
Михайлов <@DESKTOP-84VD5MI>
2023-10-20 19:32:26 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Programm
+{
+    static void Main()
+    {
+        int[] nums1 = new int[5];
+        Console.WriteLine("Введите 5 символов:");
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            nums1[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Выводятся числа в обратном попрядке");
+        for (int v = nums1.Length; v > 0; v--)
+        {
+            Console.WriteLine(v);
+        }
+        Console.ReadLine();
+    }
+}
